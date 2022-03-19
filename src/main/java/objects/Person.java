816c41d74859c8ffe1ce8ab72b404243792cc35d
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-03-19 12:29:33 -0500

    Se hicieron unas modificaciones en la clase persona, agregando unas propiedades y unos metodos

--- src/main/java/objects/Person.java
+++ src/main/java/objects/Person.java
@@ -4,10 +4,12 @@ import java.util.Date;
 
 public class Person {
     public String name;
+    public String name2;
     public String lastName;
     public String lastName2;
     public Date dateBirth;
     public float height;
+    public Integer age;
 
     public String getName() {
         return name;
@@ -15,5 +17,29 @@ public class Person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
     }
 }
