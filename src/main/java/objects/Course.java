24bd7c99188d7c7e862423ec7f5c3717cc7c2bde
Daniel Naranjo <naranjobenavidesd@gmail.com>
2022-03-19 20:57:01 -0500

    Se agrego la clase curso

---
+++ src/main/java/objects/Course.java
@@ -0,0 +1,41 @@
+package objects;
+
+public class Course {
+    public String nameCourse;
+    protected String nameTeacher;
+    private String duration;
+    public int levelComplexity;
+    protected double price;
+
+    public String getNameCourse() {
+        return nameCourse;
+    }
+
+    public void setNameCourse(String nameCourse) {
+        this.nameCourse = nameCourse;
+    }
+
+    protected double getPrice() {
+        return price;
+    }
+
+    protected void setPrice(double price) {
+        this.price = price;
+    }
+
+    private String getDuration() {
+        return duration;
+    }
+
+    private void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    private void complexityMessage(){
+        if(levelComplexity > 7 && levelComplexity <= 10){
+            System.out.println("El nivel de complejidad de este curso es: Avanzado");
+        }else{
+            System.out.println("El nivel de complejidad de este curso es: Basico");
+        }
+    }
+}
